929fa8b712a9c687456a6898d20aa8c20947a8dc
Jacky Junqi Li <jli21@rockwellautomation.com>
2023-09-27 09:11:45 +0800

    update

--- file_write.php
+++ file_write.php
@@ -1,4 +1,5 @@
 <?php
 
 $filename = 'file.txt';
-file_put_contents($filename, "1,2,3");
+file_put_contents($filename, "1,2,3".PHP_EOL);
+file_put_contents($filename, "4,5,6".PHP_EOL , FILE_APPEND | LOCK_EX);
