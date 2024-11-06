7dd2d1e9327aba7477197648d57b17da56914cf5
Jacky Junqi Li <jli21@rockwellautomation.com>
2024-11-06 13:10:02 +0800

    update

--- read_xml.php
+++ read_xml.php
@@ -1,12 +1,19 @@
 <?php
+$des_file = 'example1.xml';
+if (!copy('example.xml', $des_file)) {
+    echo "failed to copy $des_file...\n";
+    return;
+}
 
-$doc = new DOMDocument;
-$doc->load('example.xml');
 
-$de = $doc->documentElement;
+$dom = new DOMDocument;
+$dom->preserveWhiteSpace = false; // this will remove white space after node removal.
+$dom->formatOutput = true; // this will "nicely" format output on save.
+$dom->load('example1.xml');
+$doc = $dom->documentElement;
 
 // we retrieve the chapter and remove it from the book
-$chapter = $de->getElementsByTagName('chapter')->item(0);
+$chapter = $doc->getElementsByTagName('chapter')->item(0);
 if ($chapter == null)
 {
     echo "Chapter is null\n";
@@ -15,7 +22,21 @@ else
 {
     echo "Chapter is not null\n";
 }
-//$oldchapter = $de->removeChild($chapter);
 
-//echo $doc->saveXML();
+// * Query element
+$ftp_server_field = $doc->getElementsByTagName('FTPTestServer')->item(0);
+echo $ftp_server_field->nodeName . " --- " . $ftp_server_field->nodeValue;
+// * Remove element
+$doc->removeChild($ftp_server_field);
+
+// * Create element
+$ele = $dom->createElement('FTPTestServer');
+$sub_ele1 = $dom->createElement('FTPTestServerIP', '1.1.1.1');
+$ele->appendChild($sub_ele1);
+$doc->appendChild($ele);
+
+// * Save to file
+file_put_contents('example1.xml', $dom->saveXML());
+
+echo "Done.\n";
 ?>
